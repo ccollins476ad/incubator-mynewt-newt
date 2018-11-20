1c3e930e676207d11a55b28b20b1603f7e3a55a5
Christopher Collins <ccollins@apache.org>
2018-11-19 16:29:47 -0800

    more

--- newt/cli/image_cmds.go
+++ newt/cli/image_cmds.go
@@ -140,11 +140,6 @@ func resignImage(imgPath string, keys []image.ImageSigKey, keyId uint8) error {
 	return nil
 }
 
-func resignImageRunCmd(cmd *cobra.Command, args []string) {
-	NewtUsage(nil, util.NewNewtError(
-		"This command is deprecated.  Use the mimg tool to resign images."))
-}
-
 func AddImageCommands(cmd *cobra.Command) {
 	createImageHelpText := "Create an image by adding an image header to the " +
 		"binary file created for <target-name>. Version number in the header " +
@@ -196,40 +191,17 @@ func AddImageCommands(cmd *cobra.Command) {
 	cmd.AddCommand(createImageCmd)
 	AddTabCompleteFn(createImageCmd, targetList)
 
-	resignImageHelpText := "Sign/Re-sign an existing image file with the specified signing key.\nIf a signing key is not specified, the signing key in the current image\nis stripped.  "
-	resignImageHelpText += "A image header will be recreated!\n"
-	resignImageHelpText += "\nWarning: The image hash will change if you change key-id "
-	resignImageHelpText += "or the type of key used for signing.\n"
-	resignImageHelpText += "Default image format is version 1.\n"
-	resignImageHelpText += "RSA signature format by default for ver 1 image is PKCSv1.5\n"
-	resignImageHelpText += "RSA signature format for ver 2 image is RSA-PSS\n"
-
-	resignImageHelpEx := "  newt resign-image my_target1.img private.pem\n"
-	resignImageHelpEx += "  newt resign-image my_target1.img private.pem 5\n"
+	resignImageHelpText :=
+		"This command is obsolete; use the mimg tool to resign images."
 
 	resignImageCmd := &cobra.Command{
-		Use:     "resign-image <image-file> [signing-key [key-id]]",
-		Short:   "Re-sign an image.",
-		Long:    resignImageHelpText,
-		Example: resignImageHelpEx,
-		Run:     resignImageRunCmd,
+		Use:   "resign-image",
+		Short: "Obsolete",
+		Long:  resignImageHelpText,
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
 	}
-
-	resignImageCmd.PersistentFlags().BoolVarP(&newtutil.NewtForce,
-		"force", "f", false,
-		"Ignore flash overflow errors during image creation")
-	resignImageCmd.PersistentFlags().BoolVar(&image.UseRsaPss,
-		"rsa-pss", false,
-		"Use RSA-PSS instead of PKCS#1 v1.5 for RSA sig. "+
-			"Meaningful for version 1 image format.")
-	resignImageCmd.PersistentFlags().BoolVarP(&useV1,
-		"1", "1", false, "Use old image header format")
-	resignImageCmd.PersistentFlags().BoolVarP(&useV2,
-		"2", "2", false, "Use new image header format")
-	resignImageCmd.PersistentFlags().StringVarP(&encKeyFilename,
-		"encrypt", "e", "", "Encrypt image using this public key")
-	resignImageCmd.PersistentFlags().BoolVarP(&retain,
-		"retain", "r", false, "Preserve old signatures; append new ones")
 
 	cmd.AddCommand(resignImageCmd)
 }
