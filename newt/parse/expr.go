08000d92177b236c16a7b7939655697df6bb5240
Christopher Collins <ccollins@apache.org>
2019-01-31 17:56:40 -0800

    expr clenaup

--- newt/parse/expr.go
+++ newt/parse/expr.go
@@ -1,8 +1,16 @@
 package parse
 
+// ExprSet is a set of expressions.  The map value is the expression itself;
+// the key is the string representation of the expression, obtained via
+// `Node#String()`.  This is implemented as a map in this way to prevent
+// duplicate expreessions.
 type ExprSet map[string]*Node
+
+// ExprMap is a collection of named expression sets.
 type ExprMap map[string]ExprSet
 
+// Exprs returns a slice of all expressions contained in the expression set.
+// The resulting slice is sorted.
 func (es ExprSet) Exprs() []*Node {
 	if len(es) == 0 {
 		return nil
@@ -16,21 +24,52 @@ func (es ExprSet) Exprs() []*Node {
 	return nodes
 }
 
-func (es ExprSet) Disjunction() *Node {
-	return Disjunction(es.Exprs())
-}
-
-func (es ExprSet) Conjunction() *Node {
-	return Conjunction(es.Exprs())
-}
-
-func (m ExprMap) Add(api string, exprs []*Node) {
+// Add adds a set of expressions to an expression set.
+func (es ExprSet) Add(exprs []*Node) {
 	for _, e := range exprs {
-		es := m[api]
-		if es == nil {
-			es = ExprSet{}
-			m[api] = es
-		}
 		es[e.String()] = e
 	}
+}
+
+// Disjunction ORs together all the elements in an expression set, producing a
+// single expression.
+func (es ExprSet) Disjunction() *Node {
+	exprs := es.Exprs()
+
+	if len(exprs) == 0 {
+		return nil
+	}
+
+	// Recursively OR the first expression with the rest.
+	var iter func(nodes []*Node) *Node
+	iter = func(nodes []*Node) *Node {
+		if len(nodes) == 1 {
+			return nodes[0]
+		}
+
+		return &Node{
+			Code:  PARSE_OR,
+			Data:  "||",
+			Left:  nodes[0],
+			Right: iter(nodes[1:]),
+		}
+	}
+
+	return iter(exprs)
+}
+
+// Add adds a set of expressions to an expression map.
+func (m ExprMap) Add(key string, exprs []*Node) {
+	if len(exprs) == 0 {
+		return
+	}
+
+	// Create a new expression set if this is a new key.
+	es := m[key]
+	if es == nil {
+		es = ExprSet{}
+		m[key] = es
+	}
+
+	es.Add(exprs)
 }
